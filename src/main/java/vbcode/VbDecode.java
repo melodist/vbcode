f76df666e886301b4d0b7d4f94334e42bdb915ea
MELT <melt.ep00@gmail.com>
2022-02-20 11:29:25 +0900

    feat : VB Code Decoding 기능 추가

---
+++ src/main/java/vbcode/VbDecode.java
@@ -0,0 +1,27 @@
+package vbcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by melodist
+ * Date: 2022-02-20 020
+ * Time: 오전 9:41
+ */
+public class VbDecode {
+
+    public static List<Integer> vbDecode(List<Byte> encodedList) {
+        int n = 0;
+        List<Integer> decoded = new ArrayList<>();
+        for (Byte encoded : encodedList) {
+            if (encoded >= 0) {
+                n = 128 * n + encoded;
+            } else {
+                n = 128 * n + encoded + 128;
+                decoded.add(n);
+                n = 0;
+            }
+        }
+        return decoded;
+    }
+}
