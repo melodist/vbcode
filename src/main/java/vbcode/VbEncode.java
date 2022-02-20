403749928508b4551a9f560cfec69432ceecbfe8
MELT <melt.ep00@gmail.com>
2022-02-20 10:01:07 +0900

    feat : VB Code Encoding 기능 추가

--- src/main/java/vbcode/VbEncode.java
+++ src/main/java/vbcode/VbEncode.java
@@ -4,29 +4,39 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by melodist
  * Date: 2022-02-19 019
  * Time: 오후 11:39
  *
- * Pseudocode
+ *
  *
  */
 public class VbEncode {
 
-    public static List<Integer> vbEncode(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
+    public static List<Byte> vbEncode(List<Integer> numbers) {
+        List<Byte> result = new ArrayList<>();
         for (Integer number : numbers) {
-            result.add(vbEncodeNumber(number));
+            result.addAll(vbEncodeNumber(number));
         }
-        return null;
+        return result;
     }
 
-    private static Integer vbEncodeNumber(Integer number) {
-        Deque<Byte> result = new LinkedList<>();
+    public static List<Byte> vbEncodeNumber(Integer number) {
+        Integer n = number;
+        Deque<Integer> integers = new LinkedList<>();
         while (true) {
-            result.addFirst(Byte.parseByte(String.valueOf(number % 128)));
+            integers.addFirst(n % 128);
+            if (n < 128) { break; }
+            n /= 128;
         }
+        List<Integer> result = new ArrayList<>(integers);
+        int lastIndex = result.size() - 1;
+        result.set(lastIndex, result.get(lastIndex) + 128);
+        return result.stream()
+                .map(Integer::byteValue)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/vbcode/VbEncodeTest.java
+++ src/test/java/vbcode/VbEncodeTest.java
@@ -1,6 +1,16 @@
 package vbcode;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by melodist
@@ -9,4 +19,58 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class VbEncodeTest {
 
+    @ParameterizedTest
+    @MethodSource("provideNumber")
+    public void vbEncodeNumberTest(int n, List<Byte> expected){
+        // when
+        List<Byte> actual = VbEncode.vbEncodeNumber(n);
+
+        // then
+        System.out.println(actual);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideNumber() {
+        return Stream.of(
+                Arguments.of(1, List.of((byte) Integer.parseInt("10000001", 2))),
+                Arguments.of(256, List.of((byte) Integer.parseInt("00000010", 2), (byte) Integer.parseInt("10000000", 2)))
+        );
+    }
+    
+    @Test
+    public void binaryStringToIntegerTest(){
+        // given
+        String binaryString = "10000001";
+        int radix = 2;
+
+
+        // when
+        byte actual = (byte) Integer.parseInt(binaryString, radix);
+        byte excepted = -127;
+        
+        // then
+        System.out.println(Integer.toBinaryString(actual & 0xff));
+        System.out.println((byte) Integer.parseInt(binaryString, radix));
+        assertThat(actual).isEqualTo(excepted);
+    }
+
+    @Test
+    public void vbEncodeTest(){
+        // given
+        List<Integer> numbers = List.of(1, 256);
+
+        // when
+        List<Byte> actual = VbEncode.vbEncode(numbers);
+
+        // then
+        List<Byte> expectedFor1 = List.of((byte) Integer.parseInt("10000001", 2));
+        List<Byte> expectedFor256 = List.of((byte) Integer.parseInt("00000010", 2), (byte) Integer.parseInt("10000000", 2));
+
+        List<Byte> expected = Stream.of(expectedFor1, expectedFor256)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    
 }
